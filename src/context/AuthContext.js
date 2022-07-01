71d3f48cef66fc68d0a04186153f5eb692955746
herraphfx <herraph@gmail.com>
2022-07-01 12:03:36 +0400

    can add movies

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,9 +1,10 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from '../pages/firebase';
+import { auth, db } from '../pages/firebase';
 import { createUserWithEmailAndPassword, 
     signInWithEmailAndPassword, 
     signOut, onAuthStateChanged
 } from "firebase/auth";
+import {setDoc, doc} from 'firebase/firestore'
 
 const AuthContext = createContext();
 
@@ -11,7 +12,10 @@ export function AuthContextProvider({children}){
     const [user, setUser] = useState({});
 
     function signUp(email, password){
-        return createUserWithEmailAndPassword(auth, email, password);
+        createUserWithEmailAndPassword(auth, email, password);
+        setDoc(doc(db, 'users', email), {
+            savedShows: []
+        })
     }
 
     function logIn(email, password){
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const {user, logIn} = UserAuth();
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        setError('');
+        try{
+            await logIn(email,password);
+            navigate('/')
+        }catch(error){
+            console.log(error);
+            setError(error.message);
+        }
+    }
+
   return (
     <>
          <div className='w-full h-screen'>
@@ -11,10 +30,15 @@ function Login() {
             <div className='max-w-[450px] h-[600px] mx-auto bg-black/75 text-white'>
                 <div className='max-w-[320px] mx-auto py-16'>
                     <h1 className='text-3xl font-bold'>Sign In</h1>
-                    <form className='w-full flex flex-col py-4'>
-                        <input className='p-3 my-2 bg-gray-600 rounded' type='email' autoComplete='email' placeholder='Email'/>
-                        <input className='p-3 my-2 bg-gray-600 rounded' type='password' placeholder='Password' autoComplete='current-password'/>
-                        <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
+                    {error ? <p className='p-3 bg-red-400 my-3'>{error}</p>: null}
+                    <form onSubmit={handleSubmit} className='w-full flex flex-col py-4'>
+                        <input 
+                        onChange={(e) => setEmail(e.target.value)}
+                        className='p-3 my-2 bg-gray-600 rounded' type='email' autoComplete='email' placeholder='Email'/>
+                        <input 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        className='p-3 my-2 bg-gray-600 rounded' type='password' placeholder='Password' autoComplete='current-password'/>
+                        <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign In</button>
                         <div className='flex justify-between items-center text-sm text-gray-600'>
                             <p><input className='mr-2' type='checkbox'/>Remember Me</p>
                             <p>Need Help ?</p>
